9c7f29f899a22a3d700c3e229d576db93c6eec28
Silvia29 <silviasultana29@gmail.com>
2018-07-16 16:45:00 +0530

    Fourth Commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,11 +6,53 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-public count:number=0;
-  constructor(public navCtrl: NavController) {
+public count_bus1:number=0;
+public count_rick:number=0;
+public count_cart:number=0;
+public count_horse:number=0;
+public count_auto:number=0;
+public count_bus2:number=0;
+  constructor(public navCtrl: NavController,) {
 
   }
-increment(){
-  this.count+=1;
+  // ionViewDidLoad(){
+  //   this.storage.get("Count").then((data)=>{
+  //     this.count_bus1=data;
+  //   });
+  // }
+increment_bus1(){
+  this.count_bus1+=1;
+  // this.storage.set("Count",this.count_bus1);
+}
+decrement(){
+  this.count_bus1--;
+}
+
+increment_rick(){
+  this.count_rick+=1;
+}
+decrement_rick(){
+  this.count_rick--;
+}
+increment_cart(){
+  this.count_cart+=1;
+}
+decrement_cart(){
+  this.count_cart--;
+}
+increment_horse(){
+  this.count_horse+=1;
+}
+decrement_horse(){
+  this.count_horse--;
+}
+increment_auto(){
+  this.count_auto+=1;
+}
+decrement_auto(){
+  this.count_auto--;
+}
+increment_bus2(){
+  this.count_bus2+=1;
 }
 }
